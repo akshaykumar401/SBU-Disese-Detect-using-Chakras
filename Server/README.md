581c47c8b4e2b867e32e6068349768ed9c030fdc
akshaykumar401 <akshaykumar835103@gmail.com>
2026-03-29 12:39:52 +0530

    feat: integrate Google Sheets for result logging and add new disease prediction endpoint

--- Server/README.md
+++
@@ -1,5 +0,0 @@
-### Prerequisty in Server Machine
-- Check Wheter `ffmpeg` is install or not
-``` bash
- sudo apt install ffmpeg
-``` 
--- Server/app.py
+++ Server/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from feature_extractor import extract_features
 import os
 from flask_cors import CORS
+from google_sheets import insert_row
 
 app = Flask(__name__)
 CORS(app, origins="*")
@@ -37,17 +38,15 @@ def upload():
     filename = file.filename
     file.save(f'uploads/{filename}')
     
-    # Converting the Audio to mp3
-    # subprocess.run(['ffmpeg', '-i', f'uploads/{filename}', f'uploads/mp3/{filename}.mp3'])
-    
     # Extracting features
     features = extract_features(file.filename)
-
-    print(features)
+    try:
+      insert_row([features['RMS'], features['Pitch_F0'], features['Jitter'], features['Hoarseness_HNR'], features['Fatigue']])
+    except Exception as e:
+      print(e)
     
     # After Extracting Deleting the audio file from server
     os.remove(f'uploads/{filename}')
-    # os.remove(f'uploads/mp3/{filename}.mp3')
   
     return jsonify({
       'Message': "File uploaded successfully",
@@ -56,3 +55,45 @@ def upload():
       'Data': features
     }), 200
 
+# Predect Route
+@app.route('/predict', methods=['POST'])
+def predict():
+  # Checking is the audio file is present in the request
+  if 'audio' not in request.files:
+    return jsonify({
+      'Message': "No audio file part in the request",
+      'Status Code': 401,
+      'Success': False,
+      'Data': None
+    }), 401
+
+  # Get the audio file from the request
+  file = request.files['audio']
+  if file.filename == '':
+    return jsonify({
+      'Message': "No selected file",
+      'Status Code': 400,
+      'Success': False,
+      'Data': None
+    }), 400
+  
+  # Save the audio file to the server
+  if file:
+    filename = file.filename
+    file.save(f'uploads/{filename}')
+    
+    # Extracting features
+    features = extract_features(file.filename)    
+    # After Extracting Deleting the audio file from server
+    os.remove(f'uploads/{filename}')
+
+    # appending result in features
+    features['status'] = False
+    features['details'] = 'No Disease Detected'
+  
+    return jsonify({
+      'Message': "Predected Result",
+      'Status Code': 200,
+      'Success': True,
+      'Data': features
+    }), 200
