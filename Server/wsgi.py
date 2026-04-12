ea4f05cd7666dbe54da56ccd5ca0b9257a48a9ed
akshaykumar401 <akshaykumar835103@gmail.com>
2026-04-12 17:50:00 +0530

    fix: restrict numerical library threads and disable joblib multiprocessing to prevent memory spikes and semaphore leaks

--- Server/wsgi.py
+++ Server/wsgi.py
@@ -1,10 +1,18 @@
-from app import app
 import os
 from dotenv import load_dotenv
 
+# Limit underlying C-libraries threads to avoid huge memory spikes and CPU locks on Render
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+# Disable joblib multiprocessing to fix leaked semaphores warning
+os.environ["JOBLIB_MULTIPROCESSING"] = "0"
+
 load_dotenv()
 
-# Start the Flask application
+from app import app
 PORT = int(os.getenv('PORT', 5000))
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=PORT, debug=True)
